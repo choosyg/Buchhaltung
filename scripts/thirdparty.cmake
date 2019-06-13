04bcb2107439d2c91d852b90f67286a8244e7b99
Stefan Schiffler <stefan@schiffler-bremen.de>
2019-06-13 16:37:37 +0200

    Adjusted cmake default pathes

--- scripts/thirdparty.cmake
+++ scripts/thirdparty.cmake
@@ -2,7 +2,7 @@
 #############################################################
 # QT
 #############################################################
-set( CMAKE_PREFIX_PATH "C:/Qt/5.9.2/msvc2017_64" )
+set( CMAKE_PREFIX_PATH "C:/Qt/Qt5.12.3/5.12.3/msvc2017_64" )
 find_package(Qt5Core REQUIRED)
 include_directories(${Qt5Core_INCLUDE_DIRS})
 find_package(Qt5Widgets REQUIRED)
@@ -19,7 +19,7 @@ install( FILES ${QT_DIR}/Qt5Core.dll
 install( FILES ${QT_DIR}/../plugins/platforms/qwindows.dll DESTINATION ./platforms )
 file( COPY ${QT_DIR}/Qt5Core.dll DESTINATION ${PROJECT_BINARY_DIR}/app )
 file( COPY ${QT_DIR}/Qt5Widgets.dll DESTINATION ${PROJECT_BINARY_DIR}/app )
-file( COPY ${QT_DIR}/Qt5Guid.dll DESTINATION ${PROJECT_BINARY_DIR}/app )
+file( COPY ${QT_DIR}/Qt5Gui.dll DESTINATION ${PROJECT_BINARY_DIR}/app )
 file( COPY ${QT_DIR}/Qt5Svg.dll DESTINATION ${PROJECT_BINARY_DIR}/app )
 file( COPY ${QT_DIR}/Qt5PrintSupport.dll DESTINATION ${PROJECT_BINARY_DIR}/app )
 
@@ -32,7 +32,7 @@ file( COPY ${QT_DIR}/Qt5PrintSupportd.dll DESTINATION ${PROJECT_BINARY_DIR}/app 
 #############################################################
 # GTest
 #############################################################
-set( GTEST_DIR "${PROJECT_SOURCE_DIR}/../3rdparty/gtest-1.7.0" )
+set( GTEST_DIR "${PROJECT_SOURCE_DIR}/../Resources/3rd-party-windows-x86_64/gtest-vc141-1.7.0" )
 include_directories( ${GTEST_DIR}/include )
 link_directories( "${GTEST_DIR}/lib" )
 set( GTEST_LIBS
